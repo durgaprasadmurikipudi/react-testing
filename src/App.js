9f49adc1a581abd27e702fa67505d5b064cef915
Durga Prasad Murikipudi <durgaprasadmurikipudi@gmail.com>
2020-01-26 19:46:02 +0530

    intro testing complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render, shallow } from 'enzyme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+      error: false
+    }
+  }
+
+  handleDecrement = () => {
+    if(this.state.counter === 0) {
+      this.setState({error: true});
+      return;
+    }
+    this.setState(state => ({counter: state.counter - 1}));
+  }
+    
+  render() {
+    return (
+      <div data-test="component-app">
+        {this.state.error && <p style={{color: 'red'}} data-test="counter-error">The counter cannot go below 0</p>}
+        <h1 data-test="display-value">the counter value currently is {this.state.counter}</h1>
+        <button data-test="increment-button" onClick={() => this.setState(state => ({counter: state.counter + 1, error: false}))}>+</button>
+        <button data-test="decrement-button" onClick={() => this.handleDecrement()}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
